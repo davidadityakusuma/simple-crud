ef93036818ae5cf2f566685113e71aea2a2cf9ed
davidadityakusuma <suprax3620125@gmail.com>
2018-09-24 21:24:27 +0700

    Adding database file
    Adding file simple-crud.sql in root directory

---
+++ simple-crud.sql
@@ -0,0 +1,37 @@
+/*
+SQLyog Enterprise - MySQL GUI v8.05 
+MySQL - 5.5.5-10.1.35-MariaDB : Database - laravel_simplecrud
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`laravel_simplecrud` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `laravel_simplecrud`;
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id_filename` int(11) NOT NULL AUTO_INCREMENT,
+  `filename` text,
+  `name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `date_of_birth` varchar(255) DEFAULT NULL,
+  `address` text,
+  PRIMARY KEY (`id_filename`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 CHECKSUM=1 DELAY_KEY_WRITE=1 ROW_FORMAT=DYNAMIC;
+
+/*Data for the table `users` */
+
+insert  into `users`(`id_filename`,`filename`,`name`,`email`,`date_of_birth`,`address`) values (1,'david-24092018203829.txt','David Aditya','davidaditya@gmail.com','2018-09-24','malang');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
